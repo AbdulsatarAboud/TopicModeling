a2eee67b8afbf27385b481145ff487a2f7b3a5c6
AbdulsatarAboud <sataraboud@gmail.com>
2021-10-11 20:51:37 +0300

    Initial commit

---
+++ functions/functions.py
@@ -0,0 +1,22 @@
+import gensim
+from gensim.utils import simple_preprocess
+from gensim.parsing.preprocessing import STOPWORDS
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
+from nltk.stem.porter import *
+import numpy as np
+
+np.random.seed(2018)
+import nltk
+
+
+def lemmatize_stemming(text):
+    englishStemmer = SnowballStemmer("english")
+    return englishStemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v'))
+
+
+def preprocess(text):
+    result = []
+    for token in gensim.utils.simple_preprocess(text):
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
+            result.append(lemmatize_stemming(token))
+    return result
